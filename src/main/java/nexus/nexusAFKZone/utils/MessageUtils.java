87f3f500091203080c64108fe6cb330d1422268d
Santiago Lopez Mendez <s.lopzmendez@student.fdu.edu>
2025-04-05 21:12:25 -0400

    V 1.4 [ Beta ]

--- src/main/java/nexus/nexusAFKZone/utils/MessageUtils.java
+++ src/main/java/nexus/nexusAFKZone/utils/MessageUtils.java
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor;
 public class MessageUtils {
 
     public static String format(String message, Object... args) {
+        if (message == null) {
+            return "";
+        }
         return ChatColor.translateAlternateColorCodes('&', String.format(message, args));
     }
 }
